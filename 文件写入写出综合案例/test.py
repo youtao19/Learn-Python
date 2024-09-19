d0f460e2884c3403976cfc428c9523d01f376679
youtao <wuyoutao19@gmail.com>
2024-09-19 21:11:44 +0800

    2

--- 文件写入写出综合案例/test.py
+++ 文件写入写出综合案例/test.py
@@ -1,9 +1,13 @@
 f = None
 
 try:
-    f = open('文件写入写出综合案例\\test_append.txt')
+    f = open(r'文件写入写出综合案例\\test_append.txt')
     content = f.read()
     print(content)
     f.close()
 except Exception as e:
     print(e)
+
+
+from test_package import test   
+test_package.test.hello()
